3df228d7a8172edb2721df7c7e4b45a72c5c277b
Ryan Harvey <63217216+ryanharvey22@users.noreply.github.com>
2023-10-26 20:50:23 -0400

    Add files via upload

--- FileRecovery.py
+++ FileRecovery.py
@@ -3,9 +3,6 @@ import os
 import hashlib
 
 print("Checking Dependencies")
-if sys.platform != "linux":
-    print(os.system("binwalk"))
-
 if len(sys.argv) == 2:
     DISK = sys.argv[1]
 elif len(sys.argv) < 2:
@@ -15,21 +12,11 @@ elif len(sys.argv) < 2:
 else:
     print("Error, Provided too many arguments. Ignoring extra")
 
-FILE_SIGS = {
-    "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A" : "MPG",
-    "\x25\x50\x44\x46" : "PDF",
-    "\x42\x4D" : "BMP",
-    "\x47\x49\x46\x38\x37\x61" : "GIF",
-    "\x49\x46\x38\x39\x61" : "GIF",
-    "\xFF\xD8\xff\xE0" : "JPG",
-    "\x50\x4B\x03\x04\x14\x00\x06\x00": "DOCX",
-    " " : "AVI",
-    "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A": "PNG"
-}
 
-array = ["\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", "\x25\x50\x44\x46", "\x42\x4D"]
 
-# calls hexdump for given range and puts offsets and bytes into respective lists.
+############################################################################
+#   Function to ask machine for DISK bytes and offsets using hexdump tool  #
+############################################################################
 def hexdump_to_list(disk, start_sector, skip_sector):
     output = os.popen(f"hexdump -s $(({start_sector}*512)) -n $(({skip_sector}*512)) {disk}").read().split("\n")
     bytes = []
@@ -52,43 +39,59 @@ def hexdump_to_list(disk, start_sector, skip_sector):
             bytes.append(['']*16)
     return offsets, bytes
 
-def hex_list_to_ascii(x:list) -> str:
-    output = ""
-    for i in x:
-        output += chr(int(i, 16))
-    return output
-
 
 # bytes come in hexdump format
 offsets, bytes_boot = hexdump_to_list(DISK, 0, 1)
-temp = bytes_boot[0][11:13]
-bytes_per_sector = int(''.join(temp[::-1]), 16)
-
+bytes_per_sector = int(''.join(bytes_boot[0][11:13][::-1]), 16)
 sectors_per_cluster = int(bytes_boot[0][13], 16)
-
-temp = bytes_boot[0][14:]
-reserved_sectors = int(''.join(temp[::-1]), 16)
-
+reserved_sectors = int(''.join(bytes_boot[0][14:][::-1]), 16)
 num_FATs = int(bytes_boot[1][0], 16)
-
-temp = bytes_boot[1][3:5]
-num_sectors = int(''.join(temp[::-1]), 16)
+num_sectors = int(''.join(bytes_boot[1][3:5][::-1]), 16)
 if num_sectors == 0:
-    num_sectors = 65535
-
-temp = bytes_boot[1][6:8]
-num_sectors_per_FAT = int(''.join(temp[::-1]), 16)
-
-temp = bytes_boot[1][12:]
-num_sectors_before_partition = int(''.join(temp[::-1]), 16)
-
-
+    num_sectors = 65535 # OK this is definitly wrong because algorithm gets a higher offset
+num_sectors_per_FAT = int(''.join(bytes_boot[1][6:8][::-1]), 16)
+num_sectors_before_partition = int(''.join(bytes_boot[1][12:][::-1]), 16)
 sFAT = reserved_sectors
 nFAT = num_sectors_per_FAT
 offset_FAT1, bytes_FAT1 = hexdump_to_list(DISK, sFAT, nFAT)
 
-# first find sectors before first data in data section
 
+
+###################################
+#   Make List of file extentions  #
+###################################
+file_signatures = [
+    # (ext, header,..., footer)
+    ("mpg", [0x00, 0x00, 0x01, 0xBA], [0xff, 0xD9]),
+    ("pdf", [0x25, 0x50, 0x44, 0x46], [0xff, 0xD9]),
+    ("bmp", [0x42, 0x4F, 0x4F, 0x4B, 0x4D, 0x4F, 0x42, 0x49], [0xff, 0xD9]),
+    ("gif", [0x47, 0x49, 0x46, 0x38, 0x37, 0x61], [0xff, 0xD9]),
+    ("jpg", [0xFF, 0xD8, 0xFF, 0xE0], [0xff, 0xD9]),
+    ("docx",[],[0xff, 0xD9]),
+    ("avi", [],[0xff, 0xD9]),
+    ("png", [],[0xff, 0xD9]),
+]
+sroot = reserved_sectors + num_sectors_per_FAT*2
+nroot = 32
+offset_root, bytes_root = hexdump_to_list(DISK, sroot, nroot)
+
+# lets change this to use the hex signatures, go by four and look for header and footer?
+file_names = []
+for i in range(len(bytes_root)):
+    if ''.join(bytes_root[i][14:]) == '0000':  # EOS # 1-9, 9-12
+        if bytes_root[i+1][0] in ['00','e5','2e','51']:
+            name, ext = bytes_root[i+1][1:9], bytes_root[i+1][9:12]
+
+exts_test = ["jpg", "avi", "pdf", "png", "jpg", "bmp", "pdf", "jpg", "jpg", "gif", "avi", "docx", "mpg"]
+
+
+
+
+
+
+##############################################################
+#   Finds and sets the starting offset for the data section  #
+##############################################################
 count = 0
 found_start = False
 for i in range(len(bytes_FAT1)):
@@ -101,6 +104,12 @@ for i in range(len(bytes_FAT1)):
     if found_start:
         break
 
+
+
+
+#############################
+#   Finds Offsets of files  #
+#############################
 count = -count
 files = [(0, offset_FAT1[0])]
 found_end = False
@@ -119,24 +128,12 @@ for i in range(len(bytes_FAT1)):
     if found_end:
         break
 
-sroot = reserved_sectors + num_sectors_per_FAT*2
-nroot = 32
-offset_root, bytes_root = hexdump_to_list(DISK, sroot, nroot)
 
-# lets change this to use the hex signatures, go by four and look for header and footer?
-file_names = []
-for i in range(len(bytes_root)):
-    if ''.join(bytes_root[i][14:]) == '0000':  # EOS # 1-9, 9-12
-        if bytes_root[i+1][0] in ['00','e5','2e','51']:
-            name, ext = bytes_root[i+1][1:9], bytes_root[i+1][9:12]
-            file_names.append(hex_list_to_ascii(name) + "." + hex_list_to_ascii(ext))
 
-# start_of_data = reserved_sectors + num_sectors_per_FAT * 2 + 32
-# os.system(f"dd if={DISK} of=RecoveredFiles/File{i+1}.jpg bs={bytes_per_sector} skip={skip} count={length} status=none")
-# os.system(f"binwalk -R '\x25\x50\x44\x46' ")
 
-exts_test = ["jpg", "avi", "pdf", "png", "jpg", "bmp", "pdf", "jpg", "jpg", "gif", "avi", "docx", "mpg"]
-
+#############################################
+#   Displaying Results to the command line  #
+#############################################
 if not os.path.exists("RecoveredFiles"):
     os.mkdir("RecoveredFiles")
 for i in range(len(files) - 1):
