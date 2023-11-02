1f0b44415f80ad8375296c972091369bc15287ef
Ryan Harvey <63217216+ryanharvey22@users.noreply.github.com>
2023-11-02 15:13:50 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -3,22 +3,35 @@ import os
 DISK = "Project2.dd"
 
 file_signatures = [
-    ("mpg", ['00', '00', 0x01, 0xBA], [0xff, 0xD9]),
-    ("pdf", [0x25, 0x50, 0x44, 0x46], [0xff, 0xD9]),
-    ("bmp", [0x42, 0x4F, 0x4F, 0x4B, 0x4D, 0x4F, 0x42, 0x49], [0xff, 0xD9]),
-    ("gif", [0x47, 0x49, 0x46, 0x38, 0x37, 0x61], [0xff, 0xD9]),
-    ("jpg", [0xFF, 0xD8, 0xFF, 0xE0], [0xff, 0xD9]),
-    ("docx",[],[0xff, 0xD9]),
-    ("avi", [],[0xff, 0xD9]),
-    ("png", [],[0xff, 0xD9]),
+    ("jpg", ['ff', 'd8', 'ff', 'e0'], ['ff, d9']),
+    ("jpg", ['ff', 'd8', 'ff', 'e1'], ['ff, d9']),
+    ("jpg", ['ff', 'd8', 'ff', 'e8'], ['ff, d9']),
+    ("jpg", ['ff', 'd8'], ['ff, d9']),
+    ("mpg", ['00', '00', '01', 'ba'], ['00', '00', '01', 'b9']),
+    ("mpg", ['00', '00', '01', 'bX'], ['00', '00', '01', 'b7']),
+    ("gif", ['47', '49', '46', '38', '37', '61'], ['00', '3b']),
+    ("gif", ['47', '49', '46', '38', '39', '61'], ['00', '3b']),
+    ("docx",['50', '4b', '03', '04', '14', '00', '06', '00'],['50', '4b', '05', '06']),
+    ("avi", ['41', '56', '49', '20', '4c', '49', '53', '54'], []),
+    ("avi", ['52', '49', '46', '46'], []),
+    ("png", ['89', '50', '4e', '47', '0d', '0a', '1a', '0a'], ['49', '45', '4e', '44', 'ae', '42', '60', '82']),
+    ("pdf", ['25', '50', '44', '46'], ['25', '25', '45', '4f', '46']),
+    ("bmp", ['42','4d'], []),
+
 ]
 
 sig_heads = [
+    ['ff', 'd8', 'ff', 'e0'],
+    ['ff', 'd8', 'ff', 'e1'],
+    ['ff', 'd8', 'ff', 'e8'],
     ['00', '00', '01', 'ba'], 
     ['25', '50', '44', '46'], 
     ['42', '4F', '4F', '4b', '4d', '4f', '42', '49'], 
     ['47', '49', '46', '38', '37', '61'],
-    ['ff', 'd8', 'ff', 'e0']
+    ['50', '4b', '03', '04', '14', '00', '06', '00'],
+    ['41', '56', '49', '20', '4c', '49', '53', '54'],
+    ['89', '50', '4e', '47', '0d', '0a', '1a', '0a']
+
 ]
 
 sig_foots =  [
@@ -26,7 +39,7 @@ sig_foots =  [
     ['ff', 'd9'],
     ['ff', 'd9'],
     ['ff', 'd9'],
-    ['ff', 'd9']
+    ['ff', 'd9'],
 ]
 
 def hexdump_to_list(disk, start_sector, skip_sector):
@@ -55,11 +68,25 @@ def hexdump_to_list(disk, start_sector, skip_sector):
 data_section = hexdump_to_list(DISK, 448, 97661)
 byte_data = data_section[1]
 print(len(byte_data))
-for w in range(len(sig_heads)):
-    #print("checking: ", sig_heads[w])
-    for i in range(len(byte_data)):
+#print("checking: ", sig_heads[w])
+i = 0
+while i < len(byte_data) - 9:
+
+    for w in range(len(sig_heads)):
         #print("agaisnt ", byte_data[i:i+len(sig_heads[w])])
-        if sig_heads[w] == byte_data[i:i+len(sig_heads[w])]:
-            print(f"found head {sig_heads[w]} at index", i)
-        if sig_foots[w] == byte_data[i:i+len(sig_foots[w])]:
-            print("found foot at index", i)
+        if file_signatures[w][1] == byte_data[i:i+len(file_signatures[w][1])]:
+            print(f"found head {file_signatures[w]} at sector", i/512)
+
+            j = i
+            while j < len(byte_data) - 3:
+                
+                if file_signatures[w][2] == byte_data[j:j+len(file_signatures[w][2])]:
+                    print(f"found footer {file_signatures[w]} at {j/512}")
+                    break
+                j = j + 1
+
+            i = j
+
+    i = i + 1
+
+        
