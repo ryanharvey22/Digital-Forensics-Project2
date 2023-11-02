a766c75919d3f45fb089eb1f54113703c03aefd0
Ryan Harvey <63217216+ryanharvey22@users.noreply.github.com>
2023-11-02 16:20:00 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -3,43 +3,21 @@ import os
 DISK = "Project2.dd"
 
 file_signatures = [
-    ("jpg", ['ff', 'd8', 'ff', 'e0'], ['ff, d9']),
-    ("jpg", ['ff', 'd8', 'ff', 'e1'], ['ff, d9']),
-    ("jpg", ['ff', 'd8', 'ff', 'e8'], ['ff, d9']),
-    ("jpg", ['ff', 'd8'], ['ff, d9']),
+    ("jpg", ['ff', 'd8', 'ff', 'e0'], ['ff', 'd9']),
+    ("jpg", ['ff', 'd8', 'ff', 'e1'], ['ff', 'd9']),
+    ("jpg", ['ff', 'd8', 'ff', 'e8'], ['ff', 'd9']),
+    #("jpg", ['ff', 'd8'], ['ff', 'd9']),
     ("mpg", ['00', '00', '01', 'ba'], ['00', '00', '01', 'b9']),
     ("mpg", ['00', '00', '01', 'bX'], ['00', '00', '01', 'b7']),
     ("gif", ['47', '49', '46', '38', '37', '61'], ['00', '3b']),
     ("gif", ['47', '49', '46', '38', '39', '61'], ['00', '3b']),
     ("docx",['50', '4b', '03', '04', '14', '00', '06', '00'],['50', '4b', '05', '06']),
     ("avi", ['41', '56', '49', '20', '4c', '49', '53', '54'], []),
-    ("avi", ['52', '49', '46', '46'], []),
+    #("avi", ['52', '49', '46', '46'], []),
     ("png", ['89', '50', '4e', '47', '0d', '0a', '1a', '0a'], ['49', '45', '4e', '44', 'ae', '42', '60', '82']),
     ("pdf", ['25', '50', '44', '46'], ['25', '25', '45', '4f', '46']),
-    ("bmp", ['42','4d'], []),
+    #("bmp", ['42','4d'], []),
 
-]
-
-sig_heads = [
-    ['ff', 'd8', 'ff', 'e0'],
-    ['ff', 'd8', 'ff', 'e1'],
-    ['ff', 'd8', 'ff', 'e8'],
-    ['00', '00', '01', 'ba'], 
-    ['25', '50', '44', '46'], 
-    ['42', '4F', '4F', '4b', '4d', '4f', '42', '49'], 
-    ['47', '49', '46', '38', '37', '61'],
-    ['50', '4b', '03', '04', '14', '00', '06', '00'],
-    ['41', '56', '49', '20', '4c', '49', '53', '54'],
-    ['89', '50', '4e', '47', '0d', '0a', '1a', '0a']
-
-]
-
-sig_foots =  [
-    ['ff', 'd9'],
-    ['ff', 'd9'],
-    ['ff', 'd9'],
-    ['ff', 'd9'],
-    ['ff', 'd9'],
 ]
 
 def hexdump_to_list(disk, start_sector, skip_sector):
@@ -69,24 +47,39 @@ data_section = hexdump_to_list(DISK, 448, 97661)
 byte_data = data_section[1]
 print(len(byte_data))
 #print("checking: ", sig_heads[w])
-i = 0
-while i < len(byte_data) - 9:
 
-    for w in range(len(sig_heads)):
+start_offsets = []
+end_offsets = []
+
+i = 0
+em = False
+while i < len(byte_data) - 9:
+    for w in range(len(file_signatures)):
         #print("agaisnt ", byte_data[i:i+len(sig_heads[w])])
         if file_signatures[w][1] == byte_data[i:i+len(file_signatures[w][1])]:
-            print(f"found head {file_signatures[w]} at sector", i/512)
-
+            if em == True:
+                end_offsets.append(int(i/512) - 1)
+                print(f"footer {file_signatures[w]} at {int(i/512)}")
+            em = False
+            print(f"head {file_signatures[w]} at sector", int(i/512))
+            start_offsets.append(int(i/512))
             j = i
             while j < len(byte_data) - 3:
                 
-                if file_signatures[w][2] == byte_data[j:j+len(file_signatures[w][2])]:
-                    print(f"found footer {file_signatures[w]} at {j/512}")
+                if len(file_signatures[w][2]) > 0:
+                    if file_signatures[w][2] == byte_data[j:j+len(file_signatures[w][2])]:
+                        print(f"footer {file_signatures[w]} at {int(j/512)}")
+                        end_offsets.append(int(j/512))
+                        break
+                else:
+                    em = True
                     break
                 j = j + 1
-
             i = j
-
     i = i + 1
+
+for x in range(len(start_offsets)):
+    print(start_offsets[x], "->", end_offsets[x])
+
 
         
